5f0f18e07839e3c99545d7c176a73d699699146e
Mateo <6304@holbertonstudents.com>
2023-02-15 06:06:24 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
-*main - start
-*Return: always 0
-*
+/**
+ * main - start
+ * Return: always 0
+ *
 */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle")
