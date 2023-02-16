ebaadfbb7cbbb2891ad359b7a05075958aaf3834
Mateo <6304@holbertonstudents.com>
2023-02-15 17:35:12 -0800

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+	putchar(l);
+	l++;
+	}
+	putchar('\n');
+}
