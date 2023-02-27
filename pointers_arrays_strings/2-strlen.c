9ca006e47fd43ff8165a3d3fac1e393a03e16c74
Mateo <6304@holbertonstudents.com>
2023-02-27 10:55:07 -0800

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strlen - main func
+ *@s: string
+ *return: string lenght
+ */
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	if (s[lenght] != '\0')
+	{
+		lenght++;
+	}
+	return (lenght);
+}
