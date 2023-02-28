9b3ff2e1155ce36b5e54d381ef2a1ad4761bba42
Mateo <6304@holbertonstudents.com>
2023-02-28 04:28:13 -0800

    betty fix

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,11 +2,12 @@
 #include "2-strlen.c"
 /**
  *print_rev - main function
- *
+ *@s: variable
  */
 void print_rev(char *s)
 {
 	int a;
+
 	for (a = (_strlen(s) - 1); a >= 0; a--)
 	{
 		_putchar(*(s + a));
