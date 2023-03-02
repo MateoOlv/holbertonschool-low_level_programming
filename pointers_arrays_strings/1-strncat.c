9062d0c7a55c4fd66217149c2812b761ed852658
Mateo <6304@holbertonstudents.com>
2023-03-02 11:58:47 -0800

    1strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+
+		for (a = 0; a < n && src[a] = '\0'; a++)
+		{		
+		dest[a] = src[a];			
+		}
+		while (a < n)
+		{
+			a++;
+			dest[a] = '\0';
+		}
+
+	return (dest);
+}
