af07149bb2eaad3b9ad7252d8387a53f153328f2
Mateo <6304@holbertonstudents.com>
2023-03-02 12:05:15 -0800

    fix

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,12 @@ char *_strncat(char *dest, char *src, int n)
 	int a;
 
 		for (a = 0; a < n && src[a] != '\0'; a++)
-		{		
-		dest[a] = src[a];			
-		}
-		while (a < n)
 		{
-			a++;
+		dest[a] = src[a];
+		}
+		for (; a < n; a++)
+		{
 			dest[a] = '\0';
 		}
-
 	return (dest);
 }
