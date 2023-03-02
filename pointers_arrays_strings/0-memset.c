db266a3773e63ca65f8da04dc5aa63a37807a51f
Mateo <6304@holbertonstudents.com>
2023-03-02 14:51:54 -0800

    fix

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,8 +1,14 @@
 #include "main.h"
-
+/**
+ *_memset - meme main function
+ *@s: fill area
+ *@b: byte area
+ *@n: memory byte area
+ *Return: return
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int a;
+	unsigned int a;
 	
 	for (a = 0; a < n; a++)
 	{
