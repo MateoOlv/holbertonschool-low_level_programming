adf6463a32700d21411e80094da8b13b24af55d2
Mateo <6304@holbertonstudents.com>
2023-03-02 12:54:08 -0800

    fix

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	for (; a < n && src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	for (; a < n; a++)
+	{
+		dest[a] = '\0';
+	}
+	return (dest);
+}
