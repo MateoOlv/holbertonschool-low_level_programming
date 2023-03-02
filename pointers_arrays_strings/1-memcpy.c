7c3e264771664398ba31a211545750ffd0390e65
Mateo <6304@holbertonstudents.com>
2023-03-02 14:57:32 -0800

    betty fix again

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  *_memcpy - main function to copy the area
  *@dest: destination of the memory area
  *@src: source of the memory area
@@ -10,7 +9,7 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
-	
+
 	for (a = 0; a < n; a++)
 	{
 	dest[a] = src[a];
